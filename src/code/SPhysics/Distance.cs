b85978f23e40865a0ee9d493df6badf4cdc59519
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:19:50 +0100

    feat: Distance

---
+++ src/code/SPhysics/Distance.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Distance
+/// </summary>
+public static class Distance
+{
+    /// <summary>
+    /// Distance
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://math-physics-calc.com/gravitational-force-calculator">calculator</a>
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N FromGravitation<N>(N mass1, N mass2, N force, N gravitationConst)
+        where N : IRootFunctions<N>
+        =>
+        N.Sqrt(gravitationConst * mass1 * mass2 / force);
+}

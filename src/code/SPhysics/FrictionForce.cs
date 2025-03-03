1be9510343b619c928915e8f83faf2ac232275a6
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:28:48 +0100

    feat: FrictionForce

---
+++ src/code/SPhysics/FrictionForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Friction force.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Friction">wikipedia</a>
+/// </remarks>
+public static class FrictionForce
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N Static<N>(N normalForce, N staticFrictionCoef)
+        where N : INumberBase<N>
+        =>
+        normalForce * staticFrictionCoef;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N Kinetic<N>(N normalForce, N kineticFrictionCoef)
+        where N : INumberBase<N>
+        =>
+        normalForce * kineticFrictionCoef;
+}

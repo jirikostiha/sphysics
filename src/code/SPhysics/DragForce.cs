fb481290ce5bbe169e41b4bb2542f0900b1eb917
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:29:28 +0100

    feat: DragCoefficient

---
+++ src/code/SPhysics/DragForce.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace SPhysics;
+
+/// <summary>
+/// Drag force
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Drag_(physics)">wikipedia</a>
+/// <a href="https://en.wikipedia.org/wiki/Drag_equation">wikipedia</a>
+/// </remarks>
+public static class DragForce
+{
+    public static N InFluid<N>(N massDensity, N flowVelocity, N area, N dragCoef)
+        where N : INumberBase<N>
+        =>
+        (N.One / N.CreateTruncating(2)) * massDensity * (flowVelocity * flowVelocity) * area * dragCoef;
+}

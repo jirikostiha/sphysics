b575e52eb16bfde726a5a48f72b9d0e579820531
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:23:05 +0100

    feat: Pressure

---
+++ src/code/SPhysics/Pressure.cs
@@ -0,0 +1,14 @@
+﻿namespace SPhysics;
+
+/// <summary>
+/// Pressure quantity
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Pressure">wikipedia</a>
+/// </remarks>
+public static class Pressure
+{
+    public const string Name = "pressure";
+    public const string DefaultSymbol = "p";
+    public const string Dimension = Mass.Dimension+" "+Length.Dimension+"-1 "+Time.Dimension+"-2";
+}

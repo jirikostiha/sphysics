4c878c45d48e639a4c0c82b8342d9aa44cbb4ef4
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:27:21 +0100

    feat: AngularMomentum

---
+++ src/code/SPhysics/AngularMomentum.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Angular momentum
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Angular_momentum">wikipedia</a>
+/// </remarks>
+public static class AngularMomentum
+{
+    //scalar
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N Eval<N>(N mass, N velocity, N radius)
+        where N : INumberBase<N>
+        =>
+        mass * velocity * radius;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N FromMomentum<N>(N momentum, N radius)
+        where N : INumberBase<N>
+        =>
+        momentum * radius;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N FromMomentOfInertia<N>(N momentOfInertia, N angularVelocity)
+        where N : INumberBase<N>
+        =>
+        momentOfInertia * angularVelocity;
+}

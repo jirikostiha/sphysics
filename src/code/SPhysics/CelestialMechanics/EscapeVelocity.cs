30c0e3d750ceca4c983570a8956ff20ea7c9076e
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:31:11 +0100

    feat: EscapeVelocity

---
+++ src/code/SPhysics/CelestialMechanics/EscapeVelocity.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace SPhysics.CelestialMechanics;
+
+/// <summary>
+/// Escape velocity
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Escape_velocity">wikipedia</a>
+/// </remarks>
+public static class EscapeVelocity
+{
+    /// <summary>
+    /// For a spherically symmetric, massive body such as a star, or planet, the escape velocity for that body, at a given distance.
+    /// </summary>
+    public static N Eval<N>(N mass, N distance, N gravitationalConstant)
+        where N : INumberBase<N>, IRootFunctions<N>
+        =>
+        N.Sqrt(N.CreateTruncating(2) * mass * gravitationalConstant / distance);
+}

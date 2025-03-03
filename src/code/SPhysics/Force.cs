675b6b65e23d0535f976f910864a5d9b4e8e5c8d
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:22:03 +0100

    feat: Force

---
+++ src/code/SPhysics/Force.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Force quantity.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Force">wikipedia</a>
+/// </remarks>
+public static class Force
+{
+    public const string Name = "force";
+    public const string DefaultSymbol = "F";
+    public const string Dimension = Length.Dimension + " " + Mass.Dimension + " " + Time.Dimension + "-2";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N FromAcceleration<N>(N mass, N acceleration)
+        where N : INumberBase<N>
+        =>
+        mass * acceleration;
+}

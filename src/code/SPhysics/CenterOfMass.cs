4c5abd5d46192e1ffc9cfbae999c941bbbd101ec
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 19:07:04 +0100

    feat: CenterOfMass

---
+++ src/code/SPhysics/CenterOfMass.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace SPhysics;
+
+/// <summary>
+/// Center of mass or balance point.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Center_of_mass">wikipedia</a>
+/// </remarks>
+public static class CenterOfMass
+{
+    public static (N x, N y) Eval<N>((N x, N y, N m)[] points)
+        where N : INumberBase<N>
+    {
+        N totalMass = points.Aggregate(N.Zero, (sum, point) => sum + point.m);
+
+        N x = points.Aggregate(N.Zero, (sum, point) => point.x * point.m ) / totalMass;
+        N y = points.Aggregate(N.Zero, (sum, point) => point.y * point.m ) / totalMass;
+
+        return (x, y);
+    }
+}

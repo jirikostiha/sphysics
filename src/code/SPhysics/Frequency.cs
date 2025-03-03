5dc2c87457c98c81fd27bbfaf742ae50ae867637
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:21:45 +0100

    feat: Frequency

---
+++ src/code/SPhysics/Frequency.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Frequency quantity.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Frequency">wikipedia</a>
+/// </remarks>
+public static class Frequency
+{
+    public const string Name = "frequency";
+    public const string DefaultSymbol = "f";
+    public const string Dimension = Time.Dimension + "-1";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N Eval<N>(N period)
+        where N : INumberBase<N>
+        =>
+        N.One / period;
+}

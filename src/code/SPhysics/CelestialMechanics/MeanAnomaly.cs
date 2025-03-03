7e9bdf2a80871d3fb6ff6fa9390ad4be84fd345b
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:31:31 +0100

    feat: MeanAnomaly

---
+++ src/code/SPhysics/CelestialMechanics/MeanAnomaly.cs
@@ -0,0 +1,26 @@
+﻿namespace SPhysics.CelestialMechanics;
+
+using System;
+using System.Numerics;
+
+/// <summary>
+/// Mean anomaly
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Mean_anomaly">wikipedia</a>
+/// </remarks>
+public static class MeanAnomaly
+{
+    /// <summary>
+    /// From the eccentric anomaly
+    /// </summary>
+    public static N Eval<N>(N eccentricAnomaly, N eccentricity)
+        where N : INumberBase<N>, ITrigonometricFunctions<N>
+        =>
+        eccentricAnomaly - eccentricity * N.Sin(eccentricAnomaly);
+
+    public static N Eval2<N>(N longitudeOfThePericenter, N meanLongitude)
+        where N : ISubtractionOperators<N,N,N>
+        =>
+        meanLongitude - longitudeOfThePericenter;
+}

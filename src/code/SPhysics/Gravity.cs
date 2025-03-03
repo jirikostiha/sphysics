35de131484fb272dc7eed85ee67dbb9c474bbaa1
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:24:16 +0100

    feat: Gravity

---
+++ src/code/SPhysics/Gravity.cs
@@ -0,0 +1,14 @@
+﻿namespace SPhysics;
+
+/// <summary>
+/// Gravity quantity
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Gravity">wikipedia</a>
+/// </remarks>
+public static class Gravity
+{
+    public const string Name = "gravity";
+    public const string DefaultSymbol = "g";
+    public const string Dimension = Acceleration.Dimension;
+}

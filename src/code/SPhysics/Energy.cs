6bd48b69df0d4c4621b192228c305a9fad9fa1f1
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:22:33 +0100

    feat: Energy

---
+++ src/code/SPhysics/Energy.cs
@@ -0,0 +1,14 @@
+﻿namespace SPhysics;
+
+/// <summary>
+/// Energy quantity
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Energy">wikipedia</a>
+/// </remarks>
+public static class Energy
+{
+    public const string Name = "energy";
+    public const string DefaultSymbol = "E";
+    public const string Dimension = Mass.Dimension+" "+Length.Dimension+"+2 "+Time.Dimension+"-2";
+}

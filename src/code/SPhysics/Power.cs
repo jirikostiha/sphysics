a2aa8be1dcef9748b779f56eac532c3afc0be3c9
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:23:26 +0100

    feat: Power

---
+++ src/code/SPhysics/Power.cs
@@ -0,0 +1,14 @@
+﻿namespace SPhysics;
+
+/// <summary>
+/// Power quantity
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Power_(physics)">wikipedia</a>
+/// </remarks>
+public static class Power
+{
+    public const string Name = "power";
+    public const string DefaultSymbol = "P";
+    public const string Dimension = Mass.Dimension+" "+Length.Dimension+"+2 "+Time.Dimension+"-3";
+}

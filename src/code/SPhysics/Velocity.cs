a8464a0cd8368d52e585ceddacb20ec8dafda478
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:20:24 +0100

    feat: Velocity

---
+++ src/code/SPhysics/Velocity.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Velocity quantity.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Velocity">wikipedia</a>
+/// </remarks>
+public static class Velocity
+{
+    public const string Name = "velocity";
+    public const string DefaultSymbol = "v";
+    public const string Dimension = Length.Dimension + " " + Time.Dimension + "-1";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N FromAcceleration<N>(N acceleration, N time)
+        where N : INumberBase<N>
+        =>
+        acceleration * time;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N FromAngularVelocity<N>(N angularVelocity, N radius)
+        where N : INumberBase<N>
+        =>
+        angularVelocity * radius;
+}

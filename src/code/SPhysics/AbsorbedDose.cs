4f73003ccdc39c71300f289eb0d3b7e1b3635d8b
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:29:56 +0100

    feat: AbsorbedDose

---
+++ src/code/SPhysics/AbsorbedDose.cs
@@ -0,0 +1,14 @@
+﻿namespace SPhysics;
+
+/// <summary>
+/// Absorbed dose of ionizing radiation.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Absorbed_dose">wikipedia</a>
+/// </remarks>
+public static class AbsorbedDose
+{
+    public const string Name = "absorbed dose of ionizing radiation";
+    public const string DefaultSymbol = "D";
+    public const string Dimension = Length.Dimension+"+2 "+Time.Dimension+"-2"; //J * kg-1
+}

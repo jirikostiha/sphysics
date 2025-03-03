90d5e6bba3ef76043b51d591f2de32784f90d0d1
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:31:51 +0100

    feat: MeanMotion

---
+++ src/code/SPhysics/CelestialMechanics/MeanMotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace SPhysics.CelestialMechanics;
+
+/// <summary>
+/// Mean motion
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Mean_motion">wikipedia</a>
+/// </remarks>
+public static class MeanMotion
+{
+    public static N Eval<N>(N mass1, N mass2, N semiMajorAxis, N gravitationalConstant)
+        where N : INumberBase<N>, IRootFunctions<N>
+        =>
+        N.Sqrt(gravitationalConstant * (mass1 + mass2) / (semiMajorAxis * semiMajorAxis *semiMajorAxis));
+
+    public static N Eval<N>(N orbitalPeriod)
+        where N : INumberBase<N>, ITrigonometricFunctions<N>
+        =>
+        N.CreateTruncating(2) * N.Pi / orbitalPeriod;
+}

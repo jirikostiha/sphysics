fa3f24a998621adcf763630c6e07e3bee0caf0dd
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:32:34 +0100

    feat: OrbitalEccentricity

---
+++ src/code/SPhysics/CelestialMechanics/OrbitalEccentricity.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace SPhysics.CelestialMechanics;
+
+/// <summary>
+/// Orbital eccentricity
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Orbital_eccentricity">wikipedia</a>
+/// </remarks>
+public static class OrbitalEccentricity
+{
+    public static N Eval<N>(N reducedMass, N totalOrbitalEnergy, N angularMomentum, N inverseSquare)
+        where N : INumberBase<N>, IRootFunctions<N>
+        =>
+        N.Sqrt(N.One + N.CreateTruncating(2) * totalOrbitalEnergy * (angularMomentum * angularMomentum)
+            / (reducedMass * (inverseSquare * inverseSquare)));
+}

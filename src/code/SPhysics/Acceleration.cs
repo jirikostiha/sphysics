814515045a8ea41f70a1329906609805cd329c85
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:21:09 +0100

    feat: Acceleration

---
+++ src/code/SPhysics/Acceleration.cs
@@ -0,0 +1,14 @@
+﻿namespace SPhysics;
+
+/// <summary>
+/// Acceleration quantity
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Acceleration">wikipedia</a>
+/// </remarks>
+public static class Acceleration
+{
+    public const string Name = "acceleration";
+    public const string DefaultSymbol = "a";
+    public const string Dimension = Length.Dimension+" "+Time.Dimension+"-2";
+}

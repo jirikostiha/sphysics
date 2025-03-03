720992d0da2c9b95bba8059970d483151bf6a0da
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:28:22 +0100

    feat: Impulse

---
+++ src/code/SPhysics/Impulse.cs
@@ -0,0 +1,14 @@
+﻿namespace SPhysics;
+
+/// <summary>
+/// Impulse quantity.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Impulse_(physics)">wikipedia</a>
+/// </remarks>
+public static class Impulse
+{
+    public const string Name = "Impulse";
+    public const string DefaultSymbol = "J";
+    public const string Dimension = Length.Dimension + " " + Mass.Dimension + " " + Time.Dimension + "-1";
+}

3b09c37b25ec46912df13370597e5179c00ae38f
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:26:53 +0100

    feat: Momentum

---
+++ src/code/SPhysics/Momentum.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Momentum quantity.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Momentum">wikipedia</a>
+/// </remarks>
+public static class Momentum
+{
+    public const string Name = "Momentum";
+    public const string DefaultSymbol = "p";
+    public const string Dimension = Mass.Dimension + " " + Length.Dimension + " " + Time.Dimension + "-1";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N Scalar<N>(N mass, N velocity)
+        where N : INumberBase<N>
+        =>
+        mass * velocity;
+}

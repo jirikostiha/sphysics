1a86e756e19544042a834e866e488f255cbee259
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:20:53 +0100

    feat: AngularVelocity

---
+++ src/code/SPhysics/AngularVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Angular velocity.
+/// </summary>
+/// <remarks>
+/// <a href="">wikipedia</a>
+/// </remarks>
+public static class AngularVelocity
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N FromLinearVelocity<N>(N velocity, N radius)
+        where N : INumberBase<N>
+        =>
+        velocity / radius;
+}

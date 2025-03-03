9f7961b98c31cbd5485435635489af56f780a24a
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:32:57 +0100

    feat: OrbitalPeriod

---
+++ src/code/SPhysics/CelestialMechanics/OrbitalPeriod.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace SPhysics.CelestialMechanics;
+
+/// <summary>
+/// Orbital period
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Orbital_period">wikipedia</a>
+/// </remarks>
+public static class OrbitalPeriod
+{
+    /// <summary>
+    /// Small body orbiting a central body
+    /// </summary>
+    public static N Eval<N>(N mass, N semiMajorAxis, N gravitationalConstant)
+        where N : ITrigonometricFunctions<N>, IRootFunctions<N>
+        =>
+        N.CreateTruncating(2) * N.Pi * N.Sqrt(semiMajorAxis * semiMajorAxis * semiMajorAxis / (gravitationalConstant * mass));
+        //todo test
+}

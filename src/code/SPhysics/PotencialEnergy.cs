69f5a6139c989ba1731e6f0da488b35bf7ca8bde
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 21:11:23 +0100

    feat: PotencialEnergy

---
+++ src/code/SPhysics/PotencialEnergy.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace SPhysics;
+
+/// <summary>
+/// Potential energy
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Potential_energy">wikipedia</a>
+/// </remarks>
+public static class PotentialEnergy
+{
+    public static N Eval<N>(N mass, N g, N height)
+        where N : IMultiplyOperators<N,N,N>
+        =>
+        mass * g * height;
+}

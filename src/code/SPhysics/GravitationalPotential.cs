6e40bde561c5b5d8e7d7d4ed0737c724e1f43b5b
Jiri Kostiha <ijkdata@gmail.com>
2025-03-03 18:25:40 +0100

    feat: GravitationalPotential

---
+++ src/code/SPhysics/GravitationalPotential.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace SPhysics;
+
+/// <summary>
+/// Gravitational potential
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Gravitational_potential">wikipedia</a>
+/// </remarks>
+public static class GravitationalPotential
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static N Eval<N>(N mass, N distance, N gravitationConst)
+        where N : INumberBase<N>
+        =>
+        - gravitationConst * mass / distance;
+
+    //todo inside
+}
+
+///// <summary>
+///// Mass attraction potential
+///// </summary>
+///// <remarks>
+///// <a href="https://en.wikipedia.org/wiki/Gravitational_potential">wikipedia</a>
+///// </remarks>
+//public static class MassAtractionPotential //?? co jednotky?
+//{
+//    public static double f(double mass, double distance)
+//        => - mass / distance;
+//}

62a633cb1302ee518e3ab015a051220b52bfe21b
github-action-benchmark <github@users.noreply.github.com>
2026-08-24 16:04:56 +0000

    add Benchmark.Net (benchmarkdotnet) benchmark result for e0578fe99e64db120490d8f82cc4c90256648c1a

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1787587496599,
+  "repoUrl": "https://github.com/jirikostiha/sphysics",
+  "entries": {
+    "Benchmark.Net": [
+      {
+        "commit": {
+          "author": {
+            "name": "Jiri Kostiha",
+            "username": "jirikostiha",
+            "email": "ijkdata@gmail.com"
+          },
+          "committer": {
+            "name": "Jiri Kostiha",
+            "username": "jirikostiha",
+            "email": "ijkdata@gmail.com"
+          },
+          "id": "e0578fe99e64db120490d8f82cc4c90256648c1a",
+          "message": "docs: embed benchmarks dashboard iframe in documentation",
+          "timestamp": "2026-08-24T15:53:22Z",
+          "url": "https://github.com/jirikostiha/sphysics/commit/e0578fe99e64db120490d8f82cc4c90256648c1a"
+        },
+        "date": 1787587495781,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "SPhysics.GravitationalForceBenchmark.Outside",
+            "value": 14034.934572492328,
+            "unit": "ns",
+            "range": "± 10.076919178068351"
+          }
+        ]
+      }
+    ]
+  }
+}
